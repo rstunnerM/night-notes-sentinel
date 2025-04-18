3e97597596a5f2b45c8f00139d09fde5917d1aca
rstunnerM <robertomendoza10325@gmail.com>
2025-04-18 14:10:39 -0500

    Removed accidental project merge

--- ThreadEngine/blocks/UiButton.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <button @click="$emit('input', true)" class="bg-blue-500 text-white rounded px-4 py-2">
-    {{ label }}
-  </button>
-</template>
-
-<script setup>
-defineProps({ label: String })
-</script>
